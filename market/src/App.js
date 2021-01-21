cb73381635ba8a6ba1b71e3c53b26fbf0fc1dca1
Ghassan ALNomani <alnomanif15@gmail.com>
2021-01-21 13:50:53 +0300

    components file

--- market/src/App.js
+++ market/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {Header} from "./components/pages/Header"
+
+
+
+
 
 function App() {
+  //state
+
+
+  //functional
+
+
+  //JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* Start component header */}
+      <Header /> 
+      <Switch>      
+        {/* Start component main */}
+
+        {/* Start component footer */}
+      </Switch>
+    </Router>
+      
+    
   );
 }
 

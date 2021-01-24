9330bd030c4de72668dc893aa812d4a9bbe10652
Dalya <dalya-marwan@hotmail.com>
2021-01-24 21:57:05 +0300

    Home with Ghassan

--- market/src/components/pages/Home.jsx
+++ market/src/components/pages/Home.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import {CardGroup, Card, Button, Container, Row, Col} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react'
+import { CardGroup, Card, Button, Container, Row, Col } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
-export const Home = () => {
+export const Home = (props) => {
+
+     
+    const allProduct = props.product.map(ele => {
+        return (<>
+            <div className="style-card-shop">
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={ele.image} />
+                <Card.Body>
+                    <Card.Title>{ele.name}</Card.Title>
+                    <Card.Text>{ele.features}</Card.Text>
+
+                    <Button variant="primary">Add to Cart</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    </>
+    )
+    
+    })
+
+
+
     return (
         <>
             <Container>
-               <Row>
+                <Row>
                     <div className="style-form">
                         <form>
-                            <input type="search" className="style-search"/>
+                            <input type="search" className="style-search" />
                             <input type="submit" value="Search" className="style-submit colorlink" />
                         </form>
                     </div>
-               </Row>
-               <Row>
-
-               </Row>
-           </Container>
+                </Row>
+                <Row>
+                    {allProduct}
+                </Row>
+            </Container>
         </>
     )
 }
--- market/src/components/pages/Pc.jsx
+++ market/src/components/pages/Pc.jsx
@@ -1,54 +1,82 @@
 import React from 'react'
 import { Button, Container, Row, Col,Card } from 'react-bootstrap';
+import {useState} from "react"
 
 
-
-export const Pc = () => {
+export const Pc = (props) => {
 
     //state
+   //State
+   const [filter, setFilter] = useState("All")
 
 
 
 
 
-    //Functional
+   //Functional
+   const onChangehandler = (e) =>{
+       setFilter(e.target.value)
+   }
+
+   let allProduct;
+
+   if(filter === "All"){
+       allProduct = props.product.map(product=>{
+           return (
+               <div className="style-card-shop">
+                       <Card style={{ width: '18rem' }}>
+                               <Card.Img variant="top" src={product.image} />
+                               <Card.Body>
+                                   <Card.Title>{product.name}</Card.Title>
+                                   <Card.Text>
+                                       {product.features}
+                                   </Card.Text>
+                                   <Button variant="primary">Add to Cart</Button>
+                               </Card.Body>
+                        </Card>
+                   </div>
+           )
+       })
+   }else{
+       allProduct = props.product.map(product =>{
+           if(product.type === filter) return(
+               <div className="style-card-shop">
+                       <Card style={{ width: '18rem' }}>
+                               <Card.Img variant="top" src={product.image} />
+                               <Card.Body>
+                                   <Card.Title>{product.name}</Card.Title>
+                                   <Card.Text>
+                                       {product.features}
+                                   </Card.Text>
+                                   <Button variant="primary">Add to Cart</Button>
+                               </Card.Body>
+                        </Card>
+                   </div>
+           )
+       })
+   }
 
 
 
-
-
-
-    //JSX
-    return (
-        <>
-            <Container>
-                <Row>
-                    <div className="style-form">
-                        <form>
-                            <select className="style-search">
-                                <option>All</option>
-                                <option>Devices</option>
-                                <option>Games</option>
-                            </select>
-                        </form>
-                    </div>
-               </Row>
-               <Row>
-                   <div className="style-card-shop">
-                        <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Add to Cart</Button>
-                                </Card.Body>
-                         </Card>
-                    </div>
-               </Row>
-            </Container>
-        </>
-    )
+   //JSX
+   return (
+       <>
+          <Container>
+              <Row>
+                   <div className="style-form">
+                       <form>
+                           <select className="style-search" onChange={onChangehandler}>
+                               <option value="All">All</option>
+                               <option value="devices">Devices</option>
+                               <option value="games">Games</option>
+                           </select>
+                       </form>
+                   </div>
+              </Row>
+              <Row>
+                  {allProduct}
+              </Row>
+          </Container>
+       </>
+   )
 }

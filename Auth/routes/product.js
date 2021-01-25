42ec1f3e3e41723a5d8465ef28a52d3bf23ecedd
Dalya <dalya-marwan@hotmail.com>
2021-01-25 10:25:33 +0300

    nothing

--- Auth/routes/product.js
+++ Auth/routes/product.js
@@ -3,44 +3,41 @@ const router = express.Router();
 const Product = require("../models/products")
 const User = require("../models/user");
 
-router.get("/", (req, res) =>{
+
+  router.get("/", (req, res) =>{
     Product.find().then(result=>{
         res.json({result})
     })
   })
 
-router.post("/" , (req, res) => {
-    let userId = req.body.userId;
-    console.log(userId)
-    User.findById(userId)
-        .then(user=>{
-            
-        })
-        .catch(err=>res.json({msg: err}));
-
-        var addProduct ={
-            name : req.body.name,
-            discription : req.body.discription,
-            image : req.body.image,
-            price : req.body.price,
-            features : req.body.features,
-            category : req.body.category,
-            type : req.body.type,
-            company : req.body.company,
-        }
-
-            Product.create(addProduct)
-            .then((product) =>{
-                User.findByIdAndUpdate(userId, {$push:{products:product}})
-                .then((user)=>{
-                    console.log("user : " , userId)
-                    res.json({msg : "product successfully add", user})
-                })
-            })
-            .catch((err) => res.json({ msg: err }));
-        }
-    )
-
+  
+  router.post("/", (req, res) =>{
+      let userId = req.query.userId
+      var addProduct ={
+           name : req.body.name,
+           discription : req.body.discription,
+           image : req.body.image,
+           price : req.body.price,
+           features : req.body.features,
+           category : req.body.category,
+           type : req.body.type,
+           company : req.body.company,
+      }
+      console.log(addProduct)
+      Product.create(addProduct)
+      .then((product) =>{
+          console.log('new product = ', product)
+          User.findByIdAndUpdate(userId, {$push:{products: product}})
+          .populate('products')
+          .then((user)=>{
+              console.log('========================', user)
+              let populateNew = user
+              populateNew.products.push(product)
+              res.json({msg: "successfully product add", products : populateNew.products})
+          })
+          .catch(err=> res.json({msg:err}))
+      })  
+  })
 
 
 

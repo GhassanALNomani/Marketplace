5f6fb42ca2e502653c5a1e3b62f26087f3372606
Dalya <dalya-marwan@hotmail.com>
2021-01-27 15:51:18 +0300

    path in server

--- Auth/server.js
+++ Auth/server.js
@@ -20,8 +20,13 @@ app.use(express.json());
 app.use("/api/user", require("./routes/user"));
 app.use("/api/product", require("./routes/product"));
 app.use("/api/cart", require("./routes/cart"));
-          // /user
 
+
+const path = require('path')
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+})
 
 
 app.listen(PORT, () => console.log(`server running in ${PORT}`));
